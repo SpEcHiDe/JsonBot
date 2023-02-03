cde5ef4bccee74ba499b98efe072d3dfe8c232b2
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2023-02-03 12:56:15 +0530

    add some logs

--- src/handlers/msg.ts
+++ src/handlers/msg.ts
@@ -14,9 +14,11 @@ composer.on(
         "edited_channel_post",
     ],
     async (ctx) => {
+        console.log("17: ", ctx.update);
         let msgToSend = TG_MES_PR(ctx.update);
         if (msgToSend.length > TG_MAX_MESSAGE_LENGTH) {
             while (msgToSend.length > TG_MAX_MESSAGE_LENGTH) {
+                console.log("21: ", msgToSend);
                 let io: string = msgToSend.substring(0, TG_MAX_MESSAGE_LENGTH);
                 await ctx.reply(
                     `<pre><code class="language-json">${io}</code></pre>`
@@ -24,6 +26,7 @@ composer.on(
                 msgToSend = msgToSend.substring(TG_MAX_MESSAGE_LENGTH);
             }
         }
+        console.log("29: ", msgToSend);console.log("21: ", msgToSend);
         return await ctx.reply(
             `<pre><code class="language-json">${msgToSend}</code></pre>`
         );
