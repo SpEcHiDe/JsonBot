d92be7db3df27f50ba131d09d047a8e3cb494eec
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2023-02-12 21:00:29 +0530

    Handle `chat_member` updates

---
+++ src/handlers/cm.ts
@@ -0,0 +1,28 @@
+import { Composer } from "grammy/mod.ts";
+import { TG_MAX_MESSAGE_LENGTH, TG_MES_PR, TG_PR_MES } from "./../consts.ts";
+
+export const composer = new Composer();
+
+export default composer;
+
+composer.on("chat_member", async (ctx) => {
+    const targetChat = ctx.chat_member.chat.id;
+    let msgToSend = TG_MES_PR(ctx.update);
+    if (msgToSend.length > TG_MAX_MESSAGE_LENGTH) {
+        while (msgToSend.length > TG_MAX_MESSAGE_LENGTH) {
+            const io: string = msgToSend.substring(
+                0,
+                TG_MAX_MESSAGE_LENGTH,
+            );
+            await ctx.api.sendMessage(
+                targetChat,
+                TG_PR_MES(msgToSend),
+            );
+            msgToSend = msgToSend.substring(TG_MAX_MESSAGE_LENGTH);
+        }
+    }
+    return await ctx.api.sendMessage(
+        targetChat,
+        TG_PR_MES(msgToSend),
+    );
+});
