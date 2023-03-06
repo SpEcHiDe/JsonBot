83646de5caf567e62bab966d0152d706ba05986d
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2023-03-06 21:02:47 +0530

    re-writing for https://t.me/c/1493653006/116801

--- src/handlers/msg.ts
+++ src/handlers/msg.ts
@@ -1,9 +1,28 @@
-import { Composer } from "grammy/mod.ts";
+import { Composer, Context } from "grammy/mod.ts";
 import { TG_MAX_MESSAGE_LENGTH, TG_MES_PR, TG_PR_MES } from "./../consts.ts";
 
 export const composer = new Composer();
 
 export default composer;
+
+export async function msgUpdate(ctx: Context) {
+    let msgToSend = TG_MES_PR(ctx.update);
+    if (msgToSend.length > TG_MAX_MESSAGE_LENGTH) {
+        while (msgToSend.length > TG_MAX_MESSAGE_LENGTH) {
+            const io: string = msgToSend.substring(
+                0,
+                TG_MAX_MESSAGE_LENGTH,
+            );
+            await ctx.reply(
+                TG_PR_MES(io),
+            );
+            msgToSend = msgToSend.substring(TG_MAX_MESSAGE_LENGTH);
+        }
+    }
+    return await ctx.reply(
+        TG_PR_MES(msgToSend),
+    );
+}
 
 composer.on(
     // https://t.me/c/1493653006/107322
@@ -13,22 +32,5 @@ composer.on(
         "channel_post",
         "edited_channel_post",
     ],
-    async (ctx) => {
-        let msgToSend = TG_MES_PR(ctx.update);
-        if (msgToSend.length > TG_MAX_MESSAGE_LENGTH) {
-            while (msgToSend.length > TG_MAX_MESSAGE_LENGTH) {
-                const io: string = msgToSend.substring(
-                    0,
-                    TG_MAX_MESSAGE_LENGTH,
-                );
-                await ctx.reply(
-                    TG_PR_MES(io),
-                );
-                msgToSend = msgToSend.substring(TG_MAX_MESSAGE_LENGTH);
-            }
-        }
-        return await ctx.reply(
-            TG_PR_MES(msgToSend),
-        );
-    },
+    msgUpdate,
 );
