5dca657d0fc07b0cae5e8796e5dff41a81d660da
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2023-03-28 22:53:56 +0530

    forgot

--- src/handlers/cbdata.ts
+++ src/handlers/cbdata.ts
@@ -6,6 +6,15 @@ export const composer = new Composer();
 export default composer;
 
 composer.on("callback_query", (ctx) => {
+    // NOTE: You should always answer,
+    // but we want different conditionals to
+    // be able to answer to differnetly
+    // (and we can only answer once),
+
+  
+    // so we don't always answer here.
+
+    await ctx.answerCallbackQuery();
     return ctx.api.sendMessage(
         ctx.callbackQuery.from.id,
         TG_PR_MES(
